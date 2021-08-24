7ec503399344d88e6a8a1877c0c0f4ff40ea15ea
Akanshakansal01 <akansha.kansal_cs20@gla.ac.in>
2021-08-23 19:14:35 -0700

    commiting hello world

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+/*1. Write a Java program to print 'Hello' on screen and then print particular string on a separate line.
+        Expected Output :
+        Hello World
+        DDP FOR JAVA
+*/
+public class HelloWorld {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+        System.out.println("DPP FOR JAVA");
+    }
+}
+
